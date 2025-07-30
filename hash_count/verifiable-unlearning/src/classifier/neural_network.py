f28e4211d0a46baf601be0baa6da0b536eda416f
B-niroula <91728240+B-niroula@users.noreply.github.com>
2025-07-29 20:46:12 -0600

    Update neural_network.py

--- hash_count/verifiable-unlearning/src/classifier/neural_network.py
+++ hash_count/verifiable-unlearning/src/classifier/neural_network.py
@@ -8,7 +8,10 @@
 #         yield w
 
 
-# class NeuralNetwork:
+class NeuralNetwork:
+    def __init__(self, neurons=1):
+        pass
+
 
 #     def __init__(self, neurons=1):
 #         self.no_neurons = neurons
@@ -17,7 +20,7 @@
 #         self.w_1 = None
 #         self.b_1 = None
 #         self.no_features = None
-
+#
 #     def sigmoid(self, x, shift):
 #         W0 = self.add_shift(0.50, shift)
 #         W1 = self.add_shift(0.1501, shift)
@@ -25,26 +28,26 @@
 #         v = W0 + self.remove_shift(W1 * x, shift) - self.remove_shift(
 #             W3 * self.remove_shift(x * self.remove_shift(x * x, shift), shift), shift)
 #         return v
-
+#
 #     def init_model(self, config, no_features):
 #         np.random.seed(config['model_seed'])
-
+#
 #         self.no_features = no_features
-
+#
 #         self.w_0 = np.zeros((self.no_neurons, no_features)).tolist()
 #         for (j, i), w_j_i in zip(product(range(self.no_neurons), range(no_features)),
 #                                  (np.random.rand(no_features * self.no_neurons) - 0.5)):
 #             self.w_0[j][i] = self.add_shift(w_j_i, config['precision'])
-
+#
 #         self.w_1 = np.zeros((1, self.no_neurons)).tolist()
 #         for w_0_i, i in zip(np.random.rand(self.no_neurons) - 0.5, range(self.no_neurons)):
 #             self.w_1[0][i] = self.add_shift(w_0_i, config['precision'])
-
+#
 #         self.b_0 = [0] * self.no_neurons
 #         self.b_1 = [0]
-
+#
 #         return self.weights
-
+#
 #     def format_weights(self, as_zokrates, from_variable):
 #         assert as_zokrates
 #         if from_variable:
@@ -57,7 +60,7 @@
 #             weights_str += f'    u64[{self.no_neurons}] b_0 = {[0] * self.no_neurons}\n'
 #             weights_str += f'    u64 b_1 = 0'
 #             return weights_str.strip()
-
+#
 #     @property
 #     def weights(self):
 #         weights = []
@@ -66,58 +69,44 @@
 #         weights += [w_i for w in self.w_1 for w_i in w]
 #         weights += [b_i for b_i in self.b_1]
 #         return weights
-
+#
 #     def set_weights(self, weights):
 #         weights = weight_iterator(weights)
-
+#
 #         self.w_0 = np.zeros((self.no_neurons, self.no_features)).tolist()
 #         for (j, i), w_j_i in zip(product(range(self.no_neurons), range(self.no_features)), weights):
 #             self.w_0[j][i] = w_j_i
-
+#
 #         self.b_0 = [0] * self.no_neurons
 #         for i, b_0_i in zip(range(self.no_neurons), weights):
 #             self.b_0[i] = b_0_i
-
+#
 #         self.w_1 = np.zeros((1, self.no_neurons)).tolist()
 #         for i, w_0_i in zip(range(self.no_neurons), weights):
 #             self.w_1[0][i] = w_0_i
-
+#
 #         self.b_1 = list(weights)
-
+#
 #         return
-
-#     # def format_weights_init(self, config, dataset, weights_init):
-#     #     weights_init_itr = weight_iterator(weights_init)
-#     #     weights_str = ''
-#     #     for j in range(self.no_neurons):
-#     #         w_0_j = [ self.add_shift(w_i, config['precision'])
-#     #                     for i, w_i, in zip(range(dataset.no_features), weights_init_itr) ]
-#     #         weights_str += f'    u64[{dataset.no_features}] mut w_0_{j} = {w_0_j};\n'
-#     #     w_1_0 = [ self.add_shift(w_i, config['precision'])
-#     #               for i, w_i, in zip(range(self.no_neurons), weights_init_itr) ]
-#     #     weights_str += f'    u64[{self.no_neurons}] mut w_1_0 = {w_1_0};\n'
-#     #     weights_str += f'    u64[{self.no_neurons}] mut b_0 = {[0]*self.no_neurons};\n'
-#     #     weights_str += f'    u64 mut b_1 = 0;'
-#     #     return weights_str.strip()
-
+#
 #     def no_weights(self, dataset, bias=True):
 #         if bias:
 #             return self.no_neurons * (dataset.no_features + 1) + self.no_neurons + 1
 #         else:
 #             return self.no_neurons * dataset.no_features + self.no_neurons
-
+#
 #     def add_shift(self, input, shift):
 #         if shift != 1:
 #             return int(input * shift)
 #         else:
 #             return input
-
+#
 #     def remove_shift(self, input, shift):
 #         if shift != 1:
 #             return int(input / shift)
 #         else:
 #             return input
-
+#
 #     def score(self, dataset, config):
 #         no_correct = 0
 #         for x, y in zip(dataset.X_test, dataset.Y_test):
@@ -139,13 +128,11 @@
 #                     (y_pred < thresh and y == 0):
 #                 no_correct += 1
 #         return no_correct / len(dataset.X_test)
-
+#
 #     def train(self, config, dataset, weights):
-
 #         self.set_weights(weights)
-
+#
 #         for epoch in range(config['epochs']):
-
 #             loss = 0
 #             for sample_idx in range(len(dataset)):
 #                 # init accumulators
@@ -154,7 +141,6 @@
 #                 z_0 = np.zeros(self.no_neurons).tolist()
 #                 z_1 = np.zeros(1).tolist()
 #                 da_0 = np.zeros(self.no_neurons).tolist()
-#                 # da_1 = np.zeros(1).tolist()
 #                 dz_0 = np.zeros(self.no_neurons).tolist()
 #                 dz_1 = np.zeros(1).tolist()
 #                 dw_0 = [np.zeros(dataset.no_features).tolist()
@@ -201,7 +187,7 @@
 #                                                         config['precision'])
 #                 self.b_1[0] -= self.remove_shift(self.add_shift(config['lr'], config['precision']) * db_1[0],
 #                                                  config['precision'])
-
+#
 #                 for j in range(len(self.w_0)):
 #                     for i in range(len(self.w_0[j])):
 #                         self.w_0[j][i] -= self.remove_shift(
